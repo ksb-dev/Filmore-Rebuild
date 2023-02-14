b448ffb8bcf599c0616e6571cba88d75527268e4
Kedar Babaleshwar <babaleshwarkedar@gmail.com>
2023-02-14 06:51:02 +0530

    Initial Commit

--- client/src/components/MovieList/MovieList.jsx
+++ client/src/components/MovieList/MovieList.jsx
@@ -203,10 +203,10 @@ const MovieList = () => {
         {sortedMovies && sortedMovies.length > 0 && <Sort />}
         <span className='activeOption'>
           {window.location.pathname === '/watchlist' ? (
-            <span className='activeOption'>Favourite Movies</span>
+            <span className='activeOption'>Favourite movies</span>
           ) : (
             <span className='activeOption'>
-              {sessionStorage.getItem('option') + ' shows'}
+              {sessionStorage.getItem('option') + ' movies'}
             </span>
           )}
         </span>
